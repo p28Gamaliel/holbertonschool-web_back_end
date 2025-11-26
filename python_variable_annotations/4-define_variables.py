694b5e73aebdc7e7b6df4af18e31cb7045378b96
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-26 11:12:05 -0300

    define variables and annotate

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/ven python3
+"""
+define and annotate variables with the specified values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"

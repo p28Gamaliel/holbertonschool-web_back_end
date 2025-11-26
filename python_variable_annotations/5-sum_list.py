a39f78dc66647b63702550a0408b11bcfa3fcf4b
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-26 11:28:39 -0300

    complex types

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+ function sum_list which takes a list input_list of floats as argument.
+"""
+
+
+def sum_list(input_list: float) -> float:
+    return float(input_list)

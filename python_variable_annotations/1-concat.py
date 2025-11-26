56a65914d0f01f2154bbf4f41ac5e6e3676db3af
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-26 10:47:41 -0300

    concat string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+annotations of type concat.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function from concatenated strings.
+    """
+    return str1 + str2

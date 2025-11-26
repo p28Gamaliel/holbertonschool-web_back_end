b8dd7d62ce7bb1220d9c8917ca74b3d161f2bf74
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-26 16:25:04 -0300

    nose

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,19 +2,16 @@
 """
 imports.
 """
-from typing import List
 import asyncio
-from basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     async routine called wait_n that takes in 2 int.
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = []
-
     for task in asyncio.as_completed(tasks):
-        delays.append(await task)
-
+        delay = await task
+        delays.append(delay)
     return delays

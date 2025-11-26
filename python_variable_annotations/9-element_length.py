21ffd89bacbbb66a217c11f5d9ff5ae23dc515a9
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-26 12:58:19 -0300

    parameters

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,7 +7,6 @@ import typing
 
 def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """
-    Return a list of tuples where each tuple contains an element
-    from lst and its length.
+    Return a list of tuples where each tuple.
     """
     return [(i, len(i)) for i in lst]

500c3fda2c2f2f97b36216981125280557d52bca
p28gamaliel <gamaliel13perez@gmail.com>
2025-11-27 11:06:54 -0300

    random number in the list

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
 """
-imports.
+Concurrent coroutines.
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    async routine called wait_n that takes in 2 int.
+     async routine called wait_n that takes in 2 int.
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delays = []
+    delays: List[float] = []
+
     for task in asyncio.as_completed(tasks):
         delay = await task
-        delays.append(delay)
+
+        if len(delays) == 0:
+            delays.append(delay)
+        else:
+            inserted = False
+            for i in range(len(delays)):
+                if delay < delays[i]:
+                    delays.insert(i, delay)
+                    inserted = True
+                    break
+            if not inserted:
+                delays.append(delay)
+
     return delays

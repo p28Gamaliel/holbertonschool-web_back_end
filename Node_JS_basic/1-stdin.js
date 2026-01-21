0c843a70038d605f35e4f3916134e066c5838640
p28Gamaliel <gamaliel13perez@gmail.com>
2026-01-21 14:39:50 -0300

    write name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
+  console.log('This important software is now closing');
 });

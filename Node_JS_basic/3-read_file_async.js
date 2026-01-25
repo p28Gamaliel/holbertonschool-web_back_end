7759d8cbf42cc9a9ca8e2d8d46d380cf196ee0d3
p28Gamaliel <gamaliel13perez@gmail.com>
2026-01-25 16:41:02 -0300

    read file

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+      const students = lines.slice(1);
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+      for (const student of students) {
+        const [firstname, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;

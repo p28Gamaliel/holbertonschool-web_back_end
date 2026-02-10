90a52117d63adc4faad852614fc0b281dd3311f0
p28Gamaliel <gamaliel13perez@gmail.com>
2026-02-10 12:33:33 -0300

    Fix: Force LF line endings for checker

--- NoSQL/11-schools_by_topic.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-""" 11-schools_by_topic """
-
-def schools_by_topic(mongo_collection, topic):
-    """
-    Returns the list of school having a specific topic
-    """
-    return list(mongo_collection.find({ "topics": topic }))

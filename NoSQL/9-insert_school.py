40e48c710d6a03657f6f3a95ea5edc2370419427
p28Gamaliel <gamaliel13perez@gmail.com>
2026-02-10 12:43:54 -0300

    update file

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 9-insert_school """
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
